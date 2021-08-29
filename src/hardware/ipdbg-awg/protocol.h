fc8e8e3099454c447523e5e0c699af18d5aabbbe
Daniel Anselmi <danselmi@gmx.ch>
2021-08-29 21:44:31 +0200

    ipdbg awg: add implementation

--- src/hardware/ipdbg-awg/protocol.h
+++ src/hardware/ipdbg-awg/protocol.h
@@ -27,9 +27,83 @@
 
 #define LOG_PREFIX "ipdbg-awg"
 
-struct dev_context {
+struct ipdbg_awg_tcp {
+	char *address;
+	char *port;
+	int socket;
 };
 
-SR_PRIV int ipdbg_awg_receive_data(int fd, int revents, void *cb_data);
+enum {
+	IPDBG_AWG_WAVEFORM_DC = 0,
+	IPDBG_AWG_WAVEFORM_SINE,
+	IPDBG_AWG_WAVEFORM_RECTANGLE,
+	IPDBG_AWG_WAVEFORM_TRIANGLE,
+	//IPDBG_AWG_WAVEFORM_NOISE,
+	IPDBG_AWG_WAVEFORM_ARB,
+	IPDBG_AWG_NUM_WAVEFORM_TYPES
+};
+
+struct dev_context {
+	uint64_t sample_rate;
+	uint64_t center_freq;
+	double frequency;
+	//double frequency_act;
+	double amplitude;
+	double phase;
+	double offset;
+	double dutycycle;
+	int64_t *wave_buffer;
+	size_t limit_samples_max;
+	size_t limit_samples;
+	uint32_t data_width;
+	uint32_t addr_width;
+	uint32_t data_width_bytes;
+	uint32_t addr_width_bytes;
+	int waveform;
+	gboolean is_running;
+	gboolean complex_part_parallel;
+	uint8_t periods;
+};
+
+SR_PRIV struct ipdbg_awg_tcp *ipdbg_awg_tcp_new(void);
+SR_PRIV void ipdbg_awg_tcp_free(struct ipdbg_awg_tcp *tcp);
+SR_PRIV int ipdbg_awg_tcp_open(struct ipdbg_awg_tcp *tcp);
+SR_PRIV int ipdbg_awg_tcp_close(struct ipdbg_awg_tcp *tcp);
+SR_PRIV int ipdbg_awg_tcp_receive(struct ipdbg_awg_tcp *tcp,
+		uint8_t *buf, size_t bufsize);
+SR_PRIV int ipdbg_awg_send_reset(struct ipdbg_awg_tcp *tcp);
+SR_PRIV void ipdbg_awg_abort_acquisition(const struct sr_dev_inst *sdi);
+
+SR_PRIV void ipdbg_awg_get_addrwidth_and_datawidth(
+		struct ipdbg_awg_tcp *tcp, struct dev_context *devc);
+SR_PRIV void ipdbg_awg_get_isrunning(
+		struct ipdbg_awg_tcp *tcp, struct dev_context *devc);
+SR_PRIV void ipdbg_awg_init_waveform(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_update_waveform(const struct sr_dev_inst *sdi);
+SR_PRIV int ipdbg_awg_set_frequency(
+		const struct sr_dev_inst *sdi, double f_value);
+SR_PRIV double ipdbg_awg_get_frequency(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_set_amplitude(
+		const struct sr_dev_inst *sdi, double a_value);
+SR_PRIV double ipdbg_awg_get_amplitude(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_set_phase(const struct sr_dev_inst *sdi, double a_value);
+SR_PRIV double ipdbg_awg_get_phase(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_set_offset(const struct sr_dev_inst *sdi,	double o_value);
+SR_PRIV double ipdbg_awg_get_offset(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_set_dutycycle(
+		const struct sr_dev_inst *sdi, double d_value);
+SR_PRIV double ipdbg_awg_get_dutycycle(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_set_waveform(const struct sr_dev_inst *sdi, int wf_value);
+SR_PRIV const char *ipdbg_awg_waveform_to_string(int waveform);
+SR_PRIV uint64_t ipdbg_awg_get_sample_rate(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_set_sample_rate(
+		const struct sr_dev_inst *sdi, uint64_t rate);
+SR_PRIV uint64_t ipdbg_awg_get_center_freq(struct dev_context *devc);
+SR_PRIV int ipdbg_awg_set_center_freq(
+		const struct sr_dev_inst *sdi, uint64_t center_freq);
+SR_PRIV int ipdbg_awg_set_enable(const struct sr_dev_inst *sdi,	int en);
+SR_PRIV int ipdbg_awg_start(const struct sr_dev_inst *sdi);
+SR_PRIV int ipdbg_awg_stop(const struct sr_dev_inst *sdi);
 
 #endif
+
